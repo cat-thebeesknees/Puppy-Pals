b97bc5b2275da555849f25b19d9cd472915a3ed8
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-06-29 18:58:22 -0400

    completed tab 2 for assignment. set up the useState for the puppy list and assigned the p element a key

--- App.jsx
+++ App.jsx
@@ -1,17 +1,24 @@
-
-import './App.css'
+import { puppyList } from './data.js';
+import React, { useState } from 'react';
+import './App.css';
+import './index.css';
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppList: ", puppyList);
 
   return (
     <>
-      <div>
-        
+      <div className='App'>
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>;
+          })
+        }
       </div>
       
     </>
-  )
+  );
 }
 
 export default App
